06447ad500513499e65122d291e128e3851eb3ef
Jason Floriano <jasonfloriano@gmail.com>
2026-07-29 23:47:05 -0700

    Add function to test calendar connection

--- docs/js/calendar.js
+++ docs/js/calendar.js
@@ -1,1 +1,19 @@
+async function testCalendarConnection() {
 
+    try {
+
+        const response = await fetch(
+            "https://calendar.google.com/calendar/ical/571551bd31f9314f7a0a70c01d74605b594b4bc6c5951d8d72657c53c268e6a1%40group.calendar.google.com/public/basic.ics"
+        );
+
+        console.log("Calendar Status:", response.status);
+
+    } catch (error) {
+
+        console.log("Calendar Error:", error);
+
+    }
+
+}
+
+testCalendarConnection();
